4ff56425c7b6b377c61411c66a241cdc3dbeae41
Taslim A Hussain <taslim@outlook.co.nz>
2022-07-13 10:34:11 +1200

    Updated package.json

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import {uglify} from 'rollup-plugin-uglify';
-import {babel} from 'rollup-plugin-babel';
+import {babel} from '@rollup/plugin-babel';
 
 export default {
     input: "./stories/index.ts",
